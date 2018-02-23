800b8d4bab3b3928a1fe3d95d06fc2e480f6b02e
Pratik Jagrut <pratik.jagrut111@gmail.com>
2018-02-23 18:30:37 +0530

    DB Configuration

--- config/database.php
+++ config/database.php
@@ -45,7 +45,7 @@ return [
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'office_automation'),
             'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', ''),
+            'password' => env('DB_PASSWORD', '7ty9undread&6ty9$'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
