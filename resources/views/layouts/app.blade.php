3d705f10864ba8206177eb03fdb7e09846809e6e
Pratik Jagrut <pratik.jagrut111@gmail.com>
2018-02-26 15:05:28 +0530

    app.layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -62,13 +62,13 @@
                                 </a>
                                 <ul class="dropdown-menu">
                                     <li>
-                                        <a class="btn" href="{{ url('/newJobEntry') }}">Enter New Job</a>
+                                        <a href="{{ url('/newJobEntry') }}">Enter New Job</a>
                                     </li>
                                     <li>
-                                        <a class="btn" href="{{ url('listOnGoingJobs') }}">On-going Jobs</a>
+                                        <a href="{{ url('listOnGoingJobs') }}">On-going Jobs</a>
                                     </li>
                                     <li>
-                                        <a class="btn" href="{{ url('listFinishedJobs') }}">Finished Jobs</a>
+                                        <a href="{{ url('listFinishedJobs') }}">Finished Jobs</a>
                                     </li>
                                     @if (auth()->user()->user_type == 'admin')
                                         <li>
